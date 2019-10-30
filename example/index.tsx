30d58b1f9a2eb3f47db3e1a7e247ed10fcd2efca
Kunuk Nykjær <kunukn@gmail.com>
2019-10-30 21:54:59 +0100

    upd (#3)

--- example/index.tsx
+++ example/index.tsx
@@ -3,18 +3,21 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import useMount from '../.';
 //import useMount from '../dist/.';
+
+let MyComponent = ({ children }) => {
+  let onMount = node => console.log('I mounted with this node:', node);
+  let onUnmount = node => console.log('I unmounted with this  node:', node);
+  let ref = useMount({ onMount, onUnmount });
+  return <div ref={ref}>{children}</div>;
+};
+
 let App = () => {
-  let [isActive, setToggle] = React.useState(true);
-
-  let onMount = node => console.log('onMount', node);
-  let onUnMount = node => console.log('onUnMount', node);
-
-  let ref = useMount({ onMount, onUnMount });
+  let [isActive, setToggle] = React.useState(false);
 
   return (
-    <div>
-      <button onClick={() => setToggle(s => !s)}>Toggle</button>
-      {isActive && <div ref={ref}>Hello</div>}
+    <div className="App">
+      <button onClick={() => setToggle(s => !s)}>toggle</button>
+      {isActive && <MyComponent>This is my component</MyComponent>}
     </div>
   );
 };
